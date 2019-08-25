f3c5260a4f23c5b35748acda1e13e2324c441421
Ubuntu <ubuntu@ip-172-31-90-39.ec2.internal>
2019-08-25 22:49:48 +0000

    static files serve

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CPPEstimator'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/CPPEstimator/index.html'));
-});
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
