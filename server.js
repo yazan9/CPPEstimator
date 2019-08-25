001f81516e2a4d4620d72b4bbb2f678e3cf51f48
Ubuntu <ubuntu@ip-172-31-90-39.ec2.internal>
2019-08-25 22:55:31 +0000

    adding index configuration

--- server.js
+++ server.js
@@ -8,5 +8,10 @@ const app = express();
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
