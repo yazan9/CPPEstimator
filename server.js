4d19b21f6e7f31dc94f0737aa939bc0640a37e0d
Ubuntu <ubuntu@ip-172-31-90-39.ec2.internal>
2019-08-25 23:22:03 +0000

    changing static location

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(distDir));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + "/dist/index.html"));
+res.sendFile(path.join(__dirname+'/dist/estimator/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
